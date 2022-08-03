1a5ad605e9c7c63f3fa2b67b437aed1c3e356e11
Jason Mortensen <ingrey1@gmail.com>
2022-08-03 15:56:38 -0700

    update-db-setup-script

--- src/database/setup.js
+++ src/database/setup.js
@@ -3,9 +3,12 @@ const { Client } = require('pg');
 // This script was used to create the tables in the Postgres Cloud Database
 // DONT RUN THIS SCRIPT
 
+const setupDb = async () => {
+
+let client;
 
 try {
-    const client = new Client({
+    client = new Client({
       connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false
@@ -16,16 +19,16 @@ try {
 
     // 
 
-    const createBlockTableText = `CREATE TABLE IF NOT EXISTS Block (
+    const createBlockTableText = `CREATE TABLE Block(
         id serial PRIMARY KEY,
         block_hash VARCHAR(50) NOT NULL,
         height INTEGER NOT NULL,
         time  INTEGER NOT NULL,
         body VARCHAR(255) NOT NULL,
-        previous_block_hash NOT NULL
+        previous_block_hash VARCHAR(255) NOT NULL
      );`
 
-     const createStarTableText = `CREATE TABLE IF NOT EXISTS Star (
+     const createStarTableText = `CREATE TABLE Star(
         id serial PRIMARY KEY,
         star_data JSON NOT NULL
      );`
@@ -34,10 +37,16 @@ try {
 
     await client.query(createStarTableText)
     
-    client.end()
+    
 
     console.info("database tables created")
   } catch (error) {
     console.info("failed to create database tables")
     console.error(error) 
   }
+
+  client.end()
+
+}
+
+setupDb()
