a4dc81364b19c1fd4b8b2a26de2af1dc40afe298
Jason Mortensen <ingrey1@gmail.com>
2022-08-03 15:03:13 -0700

    update project structure

---
+++ src/database/query.js
@@ -0,0 +1,8 @@
+// Queries to fetch data from Postgres Database
+
+
+
+
+module.exports.database.query = {
+
+}
---
+++ src/database/setup.js
@@ -0,0 +1,3 @@
+// This script was used to create the tables in the Postgres Cloud Database
+// DONT RUN THIS SCRIPT
+
